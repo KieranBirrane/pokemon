4eb4f2f403fd471ac6f98101bddd7580c2a32b92
Kieran Birrane <kieran.d.birrane@gmail.com>
2018-11-12 22:52:18 +0000

    Update pokemon_master_list.csv

--- R_Code/web_scraping.R
+++ R_Code/web_scraping.R
@@ -14,14 +14,12 @@ natures = read.csv(file = "Standard_Data\\natures.csv", stringsAsFactors = FALSE
 ###
 
 get_pokemon_details(151,1)
-get_pokemon_details(213)
-get_pokemon_details(350)
-get_pokemon_details(358)
-get_pokemon_details(445)
-get_pokemon_details(807,1)
-
-pokemon_list_update = get_pokemon_details("Standard_Data\\pokemon_master_list.csv",213)
-
+get_pokemon_details(251,152)
+get_pokemon_details(386,252)
+get_pokemon_details(493,387)
+get_pokemon_details(649,494)
+get_pokemon_details(721,650)
+get_pokemon_details(807,722)
 
 
 prepare_pokemon_details()
